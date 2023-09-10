11381bb814000bfce21d538e4348b6d0a2443a37
Shahidulis <shahidulislam47326@gmail.com>
2023-09-11 02:12:06 +0600

    it's ready

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      screens: {
+        'sp': '480px'
+      }
+    },
+  },
+  plugins: [],
+}
+
